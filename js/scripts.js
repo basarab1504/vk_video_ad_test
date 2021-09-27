dc9491aa1fd5ca6385cfa683cc156936188c9bc8
Vlad Petrushin <joker-power@yandex.ru>
2021-09-27 15:07:00 +0300

    ad

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
-console.log("helloooooooo");
-
 vkBridge.send('VKWebAppInit');
 
 // Подписка на событие-результат
 vkBridge.subscribe((e) => {
     console.log(e);
 });
+
+function checkAd() {
+    console.log("checkAd");
+    vkBridge.send("VKWebAppCheckNativeAds", { "ad_format": "reward" });
+}
+
+function showAd() {
+    console.log("showAd");
+    vkBridge.send("VKWebAppShowNativeAds", { "ad_format": "reward" });
+}
